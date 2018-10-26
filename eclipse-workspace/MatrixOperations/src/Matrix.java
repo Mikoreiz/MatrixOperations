4da7a69545c8405ef6e8abc07eef9aaeb8c83ca8
Mikhael Oreiro <mikhaeloreiro@yahoo.com>
2018-10-26 15:38:59 -0700

    Update Matrix.java

--- eclipse-workspace/MatrixOperations/src/Matrix.java
+++ eclipse-workspace/MatrixOperations/src/Matrix.java
@@ -1,8 +1,43 @@
 
 public class Matrix {
 
-	public Matrix() {
+	final int[] matrix;
+	final int row, column;
+	
+	public Matrix(int[] Nmatrix, int Nrow, int Ncolumn) {
 		// TODO Auto-generated constructor stub
+		matrix = Nmatrix;
+		row = Nrow;
+		column = Ncolumn;
 	}
-
+	public static boolean isSquareMatrix(Matrix mat) {
+		// Determines whether matrix is a square matrix
+		if (mat.row == mat.column)
+			return true;
+		else 
+			return false;
+	}
+	public static int[] add(Matrix one, Matrix two) {
+		//Adds two matrixes together
+		int[] aux = new int[one.matrix.length-1];
+		if (one.row == two.row && one.column == two.column) {
+			for (int element=0; element <= one.matrix.length-1; element++)
+				aux[element] = one.matrix[element] + two.matrix[element];
+			return aux;
+			}
+		else
+			return aux;
+	}
+	public static int[] sub(Matrix one, Matrix two) {
+		//Subtracts two matrixes together
+		int[] aux = new int[one.matrix.length-1];
+		if (one.row == two.row && one.column == two.column) {
+			for (int element=0; element <= one.matrix.length-1; element++)
+				aux[element] = one.matrix[element] - two.matrix[element];
+			return aux;
+			}
+		else
+			return aux;
+	}
+	
 }
